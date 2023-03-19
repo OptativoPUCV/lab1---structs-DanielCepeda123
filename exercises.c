4c8ca000f5440c4976844632d7c7bf941b0fffc1
Replit user <>
2023-03-19 02:23:08 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -140,7 +140,7 @@ void sumaV(Vector * a, Vector * b, Vector * c)
 {
   int i;
 
-  for ( i = 0 ; i <= a->capacidad ; i++)
+  for ( i = 0 ; i < a->capacidad ; i++)
     {
       c->datos[i] = a->datos[i] + b->datos[i];
     }
@@ -155,6 +155,8 @@ sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c)
 {
-  
-  
+   Vector a;
+   Vector b;
+
+    
 }
